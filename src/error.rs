582decb3a9bbe4c705a5ca563fad4bc81930cff9
Jesse A. Tov <jesse.tov@gmail.com>
2018-05-12 00:24:15 -0500

    impl ::std::error::Error for Error

--- src/error.rs
+++ src/error.rs
@@ -6,7 +6,6 @@ pub type Result<T> = result::Result<T, Error>;
 /// The error type for argument parser.
 #[derive(Clone, Debug, PartialEq)]
 pub struct Error {
-    original:   String,
     message:    String,
 }
 
@@ -14,23 +13,28 @@ impl Error {
     /// Creates an argument error from any type that can be stringified.
     pub fn from_string<S: ToString + ?Sized>(e: &S) -> Self {
         Error {
-            original:   String::new(),
-            message:    e.to_string(),
+            message:   e.to_string(),
         }
     }
 
     /// Sets the particular option that triggered the error.
     pub fn with_option<S: Into<String>>(mut self, option: S) -> Self {
-        self.original = option.into();
+        if !self.message.starts_with("option -") {
+            self.message = format!("option {}: {}", option.into(), self.message);
+        }
+
         self
+    }
+}
+
+impl ::std::error::Error for Error {
+    fn description(&self) -> &str {
+        &self.message
     }
 }
 
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        if !self.original.is_empty() {
-            write!(f, "option {}: ", self.original)?;
-        }
-        f.write_str(&self.message)
+        write!(f, "{}", self.message)
     }
 }
